062af0c916e2cd20584760761d467e867520e3d3
letdayy <lcdayane49@gmail.com>
2024-07-08 08:34:20 -0300

    feat: configure login screen

---
+++ ChatApp/Module/Controller/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  ChatApp
+//
+//  Created by leticia.dayane on 08/07/24.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        title = "Login"
+    }
+    
+    //MARK: - Helpers
+}
---
+++ ChatApp/Module/Controller/RegisterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterViewController.swift
+//  ChatApp
+//
+//  Created by leticia.dayane on 08/07/24.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+    
+    //MARK: - Helpers
+}
